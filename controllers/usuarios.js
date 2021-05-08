d56c05d46c8c9e696dc56aa85bacf114c5daf2db
Bernabe Carcamo <karkamoz@gmail.com>
2021-05-07 22:36:19 -0500

    control de parametros y querys

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,20 +1,30 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+	const { q, nombre = null, apikey } = req.query;
+
 	res.json({
-		msg: 'get API - controlador'
+		msg: 'get API - controlador',
+		q,
+		nombre,
+		apikey
 	});
 };
 
 const usuariosPost = (req, res = response) => {
+	const body = req.body;
 	res.json({
-		msg: 'post API - controlador'
+		msg: 'post API - controlador',
+		body
 	});
 };
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = (req = request, res = response) => {
+	const { id } = req.params;
+
 	res.json({
-		msg: 'put API - controlador'
+		msg: 'put API - controlador',
+		id
 	});
 };
 
