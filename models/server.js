f76cc7ddeed2af75b3db9173c171d50a8894afad
Bernabe Carcamo <karkamoz@gmail.com>
2021-05-07 19:03:39 -0500

    base node rest server

---
+++ models/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+class Server {
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT;
+		this.pathUsuarios = '/api/usuarios';
+
+		this.middlewares();
+
+		this.routes();
+	}
+
+	middlewares() {
+		this.app.use(cors());
+		this.app.use(express.static('public'));
+	}
+
+	routes() {
+		this.app.use(this.pathUsuarios, require('../routes/usuarios'));
+	}
+
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log('Servidor corriendo en puerto:', this.port);
+		});
+	}
+}
+
+module.exports = Server;
